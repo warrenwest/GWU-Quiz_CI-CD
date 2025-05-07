05df7f13f659a5b7088762dc6bb784c6c039c4a9
Wren West <wrenwestart@gmail.com>
2025-05-07 10:05:49 -0400

    edited package.json

--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -2,7 +2,7 @@ import db from "../config/connection.js";
 import Question from "../models/Question.js";
 import cleanDB from "./cleanDb.js";
 
-import pythonQuestions from './pythonQuestions.json' assert { type: "json" };
+import pythonQuestions from './pythonQuestions.json' assert { type: 'json' };
 
 db.once('open', async () => {
   await cleanDB('Question', 'questions');
