5eab00a5300ec5ceb6fdac3d3483ff6098673bf3
Wren West <wrenwestart@gmail.com>
2025-05-07 10:13:53 -0400

    removed type: module from package.json and edited seed.ts

--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -2,7 +2,7 @@ import db from "../config/connection.js";
 import Question from "../models/Question.js";
 import cleanDB from "./cleanDb.js";
 
-import pythonQuestions from './pythonQuestions.json';
+const pythonQuestions = require('./pythonQuestions.json');
 
 
 db.once('open', async () => {
